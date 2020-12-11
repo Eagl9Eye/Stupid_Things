7a7cc18b52fa35cf1e435fab25c40d8114d27ed9
Eagl9Eye <32015960+Eagl9Eye@users.noreply.github.com>
2020-12-11 19:29:15 +0100

    Clean Filename added

---
+++ FilenameCleaner/CleanFileame.ps1
@@ -0,0 +1,59 @@
+﻿$path = $args[0]
+Function rename-file($path, $new_name){
+	try{
+		Rename-Item -LiteralPath $path $new_name -ErrorAction Stop
+		return $TRUE
+	} catch {
+		return $FALSE
+	}
+}
+
+Function clean-name($old_name){
+	[hashtable[]]$rules = @(
+		@{"origin"=" "; "replacement"="_"},
+		@{"origin"="ä"; "replacement"="ae"},
+		@{"origin"="ü"; "replacement"="ue"},
+		@{"origin"="ö"; "replacement"="oe"},
+		@{"origin"="Ä"; "replacement"="Ae"},
+		@{"origin"="Ü"; "replacement"="Ue"},
+		@{"origin"="Ö"; "replacement"="Oe"},
+		@{"origin"="\?"; "replacement"=""},
+		@{"origin"="!"; "replacement"=""},
+		@{"origin"="~"; "replacement"=""},
+		@{"origin"="-"; "replacement"="_"},
+		@{"origin"="\+"; "replacement"=""},
+		@{"origin"="#"; "replacement"="_"},
+		@{"origin"="\("; "replacement"="["},
+		@{"origin"="@{"; "replacement"="["},
+		@{"origin"="\)"; "replacement"="]"},
+		@{"origin"="}"; "replacement"="]"})
+
+	$name = $old_name -match "\\(?<filename>[^\\]+\.\w+)"
+	$new_name = "$($matches.filename)"
+
+	#apply rules
+	foreach($r in $rules)
+	{
+		$new_name = $new_name -creplace $r["origin"],$r["replacement"]
+	}
+	#rename file
+	$result = $FALSE
+	$i = 0
+	$extension = ""
+	$name = ""
+	while(!($result)){
+		$result = rename-file -path $path -new_name $new_name
+		$parts = $new_name.split(".") #apply number to name of file
+		$name = $parts[0]
+		$extension = $parts[1]
+		$i += 1
+		if($i -ge 2){
+			$name = $name.substring(0,$name.length-2)
+		}
+		$name = "${name}_${i}"
+		$new_name = "$($name).$($extension)"
+	}
+	#Add-Type -AssemblyName PresentationFramework
+	#[System.Windows.MessageBox]::Show("Hello $path $(Split-Path -Path $path)\$new_name $new_name")
+}
+clean-name -old_name $path
---
+++ FilenameCleaner/ReadMe.md
@@ -0,0 +1,15 @@
+# Clean File Name
+
+Use this script to rename your files automaticly
+
+## Installation
+
+Move `CleanFileName.ps1` to `C:\scripts\` and execute `~\CleanFileName_Install.reg`
+
+## Usage
+
+Right-click on the file you want to rename and choose `CleanFileName` in the context menue
+
+## Unsinstall
+
+Execute `~\CleanFileName_Remove.reg`
